82df99c26f1980d5b5b16451e082fb3ee949d810
HarshithaBoppudi <67325628+HarshithaBoppudi@users.noreply.github.com>
2021-05-20 11:03:58 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { render } from 'react-dom';
+import { StyleSheet, Text, View } from 'react-native';
+import ScanScreen from './screens/ScanScreen';
+
+export default class App extends React.Component {
+  render(){
+    return(
+     
+     <View>
+       <ScanScreen/>
+     </View>
+      
+    )
+  }
+  }
